eb11546e8075d487954e1053340cdca0399eccd1
Daniel <38474652+Dspiwak@users.noreply.github.com>
2021-01-29 01:03:12 -0500

    Tweaked dataframe output, and adjusted the way the ggplot map is displayed to not "remove data" which resulted in a warning, and appeared to be causing problems.
    Added functionality to remove currently displayed row from dataframe if the user clicks "backwards"
    
    Found an interesting bug where sometimes, the selected tree is written multiple times if the user clicks forward and backwards multiple times.

--- Scripts/ManualPointMatching_Shiny.R
+++ Scripts/ManualPointMatching_Shiny.R
@@ -10,7 +10,7 @@ library(rgeos)
 library(sf)
 
 
-
+#perform initial preprocessing of shapefiles to be loaded into app (hardcoded)
 chulldata<-readOGR("C:/Users/SUPER4/Desktop/FilesForColin/Calculated_Chulls.shp") %>%
   spTransform("+proj=utm +zone=17 +ellps=GRS80 +units=m +no_defs")
 chulldata$centx<-NA
@@ -38,7 +38,7 @@ ui<-navbarPage(
   tabPanel("Load Data",
            
            #chull UI element
-           fileInput('chapefile','Choose LiDAR Convex Hulls',multiple=FALSE,accept="shp"),
+           fileInput('shapefile','Choose LiDAR Convex Hulls',multiple=FALSE,accept="shp"),
            fluidRow(column(4,verbatimTextOutput("chull_fname")))
   ),
   
@@ -62,7 +62,8 @@ ui<-navbarPage(
         #Output: Table summarizing the values eneterd----
         plotOutput("map",click="click"),
         verbatimTextOutput("info"),
-        tableOutput("MatchedTreesTable")
+        tableOutput("MatchedTreesTable"),
+        downloadButton('downloadData','Export Dataframe (.csv)')
         )
     )
 )
@@ -79,12 +80,14 @@ server<-function(input,output,session){
   
   #show values in an html table----
   output$values<-renderTable({
-    sliderValues() 
+    sliderValues() #confidence values
   })
   
   featnum<-reactiveVal(0) #sets up a reactive variable to allow for iteratation through rows in DFs
   observeEvent(input$iterfeature,featnum(featnum()+1)) #iterates to next feature
-  observeEvent(input$inv_iterfeature,featnum(featnum()-1))
+  observeEvent(input$inv_iterfeature,{
+               featnum(featnum()-1)
+               df$dt<-df$dt[-nrow(df$dt),]})#iterates backwards
   observeEvent(input$nomatch,featnum(featnum()+1)) #skips feature if clicks no match present
   
   
@@ -98,72 +101,88 @@ server<-function(input,output,session){
   
   #Create a ggplot 'map' that plots the currently selected feature and ground truth w/ centroids
     output$map<-renderPlot({
-      if(featnum()==0) return()
+      if(featnum()==0) return()#will not display a "map" on first iteration where no features exist
       input$map
       
-      allbuffs<-gtruthdata[gtruth$buffs,,drop=FALSE]
-      selectedbuffs<-gtruthdata[!gtruth$buffs,,drop=FALSE]
+      allbuffs<-gtruthdata[gtruth$buffs,,drop=FALSE]#stores within current widget all available gtruth buffers
+      selectedbuffs<-gtruthdata[!gtruth$buffs,,drop=FALSE]#stores within current widget all subsetted gtruth buffers
       
       
       map<-ggplot()+
-        geom_sf(data=chulldata[featnum(),],fill='red')+
-        geom_sf(data=allbuffs,fill='blue')+
-        geom_sf(data=selectedbuffs,fill='green')+
-        geom_point(data=data.frame(gtruthcents),aes(x=data.frame(gtruthcents@coords)$x,y=data.frame(gtruthcents@coords)$y,colour='pink'))+
-        coord_sf(datum=st_crs(chulldata))+ #sets projection of the ggplot graph
-        xlim((st_bbox(chulldata[featnum(),])$xmin)-2,(st_bbox(chulldata[featnum(),])$xmax)+2)+ #edits the extents of the graph
-        ylim((st_bbox(chulldata[featnum(),])$ymin)-2,(st_bbox(chulldata[featnum(),])$ymax)+2)
+        geom_sf(data=chulldata[featnum(),],fill='red')+#add layer for convex hulls
+        geom_sf(data=allbuffs,fill='blue')+#add layer for gtruth buffers
+        geom_sf(data=selectedbuffs,fill='green')+#add layer for highlighted (selected) gtruth buffers
+        geom_point(data=data.frame(gtruthcents),aes(x=data.frame(gtruthcents@coords)$x,y=data.frame(gtruthcents@coords)$y,colour='pink'))+ #plots centroid (must be clicking within 20pixels of this feature to "match" the gtruth)
+        coord_sf(datum=st_crs(chulldata), #sets projection of the ggplot graph
+          xlim=c((st_bbox(chulldata[featnum(),])$xmin)-2,(st_bbox(chulldata[featnum(),])$xmax)+2), #edits the extents of the graph
+          ylim=c((st_bbox(chulldata[featnum(),])$ymin)-2,(st_bbox(chulldata[featnum(),])$ymax)+2))
       map
     })
 
-    observeEvent(input$click,{
-      res<-nearPoints(data.frame(gtruthcents),input$click,xvar='x',yvar='y',maxpoints=1,threshold=20,allRows=TRUE)
-      gtruth$buffs<-xor(gtruth$buffs,res$selected_)
+    observeEvent(input$click,{ #When clicking on a feature
+      res<-nearPoints(data.frame(gtruthcents),input$click,xvar='x',yvar='y',maxpoints=1,threshold=20,allRows=TRUE)#detect a centerpoint of a gtruth buffer within 20 pixels
+      gtruth$buffs<-xor(gtruth$buffs,res$selected_)#then subset the gtruth data based on this selected centroid
     })
     
+    
+    
+    #Build dataframe based off of clicked ground truth stems
     vals<-reactiveValues(
-      chulls=NULL,
+      chulls=NULL,#initializes these to NULL, but will be updated once running
       truths=NULL
     )
     observe({
-      vals$chulls<-chulldata[featnum(),]$diam
+      vals$chulls<-chulldata[featnum(),]#Makes the reactive chulls values under vals= to the subset of the dataframe based on the current iteration being displayed
     })
-    observeEvent(input$click,{vals$truths<-gtruthdata[!gtruth$buffs,,drop=TRUE]$treeID})
+    observeEvent(input$click,{vals$truths<-gtruthdata[!gtruth$buffs,,drop=TRUE]})#based on a clicked stem, assign the clicked stem (as a subset from the ground truth) to the reactive value under vals
     
-    df<-reactiveValues()
-    df$dt<-data.frame(ID=as.numeric(),
-                      chulldiam=as.numeric(),
+    df<-reactiveValues() #makes a reactive dataframe to store values during matching process
+    df$dt<-data.frame(Gtruth_ID=as.numeric(),
+                      Gtruth_SP=as.character(),
+                      Gtruth_dbh_cm=as.numeric(),
+                      Gtruth_NADX=as.numeric(),
+                      GTRUTHNADY=as.numeric(),
+                      Chull_diam=as.numeric(),
+                      Chull_centx=as.numeric(),
+                      Chull_centy=as.numeric(),
+                      Confidence=as.integer(),
                       iteration=as.numeric())
     
-    observeEvent(input$iterfeature,{
+    observeEvent(input$iterfeature,{#When the iterfeature button is clicked, and if it is not the first "iteration" (no data present) it will write subsetted reactive data to this row in the dataframe
       if(featnum()>1){
-        row<-data.frame(ID=vals$truths,
-                        chulldiam=vals$chulls,
+        row<-data.frame(Gtruth_ID=vals$truths$treeID,#builds row
+                        Gtruth_SP=vals$truths$sp,
+                        Gtruth_dbh_cm=vals$truths$dbh_cm,
+                        Gtruth_NADX=vals$truths$NADX,
+                        Gtruth_NADY=vals$truths$NADY,
+                        Chull_diam=vals$chulls$diam,
+                        Chull_centx=vals$chulls$centx,
+                        Chull_centy=vals$chulls$centy,
+                        Confidence=sliderValues(),
                         iteration=featnum())
-        df$dt<-rbind(df$dt,row)
-        df$dt<-df$dt[!duplicated(df$dt$iteration),]
+        df$dt<-rbind(df$dt,row) #binds the row to reactive dataframe
+        df$dt<-df$dt[!duplicated(df$dt$iteration),]#removes "duplicate rows"  that are created during this process to ensure only 1 iteration writes 1 row
       }
     })
     
     
     
-    output$info<-renderText({
+    output$info<-renderText({ #output general text for testing purposes primarily
       paste0("Selected Tree ID : x=",gtruthdata[!gtruth$buffs,,drop=FALSE]$treeID, #output window of information
              "\nCurrent Chull Feature # :",featnum(),
-             "teststuff: ", vals$chulls,
-             "teststuff2: ",vals$truths)
+             "\nteststuff: ", vals$chulls$tempid,
+             "\nteststuff2: ",vals$truths$treeID)
     })
     
-    output$"MatchedTreesTable"<-renderTable(df$dt)
+    output$downloadData<-downloadHandler(
+      filename=function(){
+        paste("Manually_Matched_Trees_Dataset",".csv",sep="")},
+      content=function(file){
+        write.csv(df$dt,file,row.names=FALSE)
+        })
+    
+    output$"MatchedTreesTable"<-renderTable(df$dt) #display the dataframe being built (this will be re-rendered and updated at each iteration)
     
 }
 ####--------RUN APP-----------------------------------------
 shinyApp(ui,server)
-
-
-temp<-(read.csv("C:/Users/note2/Downloads/Generated (2).csv"))
-testemp<-mutate(temp,plotval=1)
-spdf<-SpatialPoints(testemp[,1:2],data=as.data.frame(testemp$plotval))
-ras<-raster(spdf)
-plot(ras)
-plot(spdf)
