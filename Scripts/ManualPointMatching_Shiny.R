92f42fd88be34110013085b1a91184a1e3b795f4
Daniel <38474652+Dspiwak@users.noreply.github.com>
2021-02-02 22:50:45 -0500

    Improved UI, and reorganized to condense variables to make more readible.
    Found bug which sometimes occurs when trying to "skip" features and going forwards and backwards multiple times. Trying to located.
    
    General functionality exists.

--- Scripts/ManualPointMatching_Shiny.R
+++ Scripts/ManualPointMatching_Shiny.R
@@ -25,31 +25,31 @@ ui<-navbarPage(theme=shinytheme("darkly"),
   
   #create tab for matching stems game
   tabPanel("Match Stems",
-           fluidRow(
-             column(12,
-                    wellPanel(
-                      #Output: Table summarizing the values eneterd--
-                      plotOutput("map",click="click"),
-                      fluidRow(
-                        column(6,actionButton('inv_iterfeature','Previous Convex Hull Feature',position='right'),
-                               actionButton('iterfeature','Next Convex Hull Feature',position='left'),offset=6)),
-                      fluidRow(
-                        column(2,actionButton('nomatch','No Match Present, Skip Feature'),offset=4)),
-                      verbatimTextOutput("info")
+           wellPanel(
+                  fluidRow(
+                    column(12,
+                        #Output: Table summarizing the values eneterd--
+                        plotOutput("map",click="click")),
+                  fluidRow(
+                    column(1,actionButton('inv_iterfeature','<<---- Previous Convex Hull Feature'),offset=2),
+                    column(2,radioButtons("integer","Confidence: ( 1 Low - High 5)",
+                                 c('1'=1,
+                                   '2'=2,
+                                   '3'=3,
+                                   '4'=4,
+                                   '5'=5),
+                                 inline=TRUE),offset=2),
+                    column(1,actionButton('iterfeature','Next Convex Hull Feature ---->>'),position='left',offset=1)),
+                  fluidRow(
+                    column(1,actionButton('nomatch','No Match Present (Skip)'),offset=5)),
+                        verbatimTextOutput("info")
                       )
-                    )
              ),
       #first sidebar panel--
       fluidRow(
         column(8,
                wellPanel(
                  #create widget in first panel--
-                 radioButtons("integer","Confidence:",
-                              c('1'=1,
-                                '2'=2,
-                                '3'=3,
-                                '4'=4,
-                                '5'=5)),
                  tableOutput("MatchedTreesTable"),
                  downloadButton('downloadData','Export Dataframe (.csv)'),
                  useShinyjs()
@@ -62,8 +62,11 @@ ui<-navbarPage(theme=shinytheme("darkly"),
 ####--------------SERVER-----------------------------------------------------------------
 
 server<-function(input,output,session){
-  data<-reactiveValues(importfile=NULL,importfile2=NULL)
+
+  #Data Importation Controls----
+  data<-reactiveValues(importfile=NULL,importfile2=NULL) #Creates reactive value which "holds" the two imported shapefiles
   
+  #Chull data importation
   observeEvent(input$LiDAR_Data,{
     shpdf<-input$LiDAR_Data
     tempdir<-dirname(shpdf$datapath[1])
@@ -77,6 +80,7 @@ server<-function(input,output,session){
     data[['importfile']]$tempid<-1:nrow(data[['importfile']])
     data[['importfile']]<-st_as_sf(data[['importfile']])
   })
+  #Ground truth data importation
   observeEvent(input$Ground_Truths_Data,{
     shpdf<-input$Ground_Truths_Data
     tempdir<-dirname(shpdf$datapath[1])
@@ -90,87 +94,90 @@ server<-function(input,output,session){
       st_as_sf()
   })
   
-  #Create 'confidence' table to allow user to enter in confidence value----
-  sliderValues<-reactive({
-    data.frame(Name=c("Confidence"),
-               Value=as.character(c(input$integer)),
-               stringsAsFactors=FALSE)
-  })
   
-  #show values in an html table----
-  output$values<-renderTable({
-    sliderValues() #confidence values
-  })
-  
+  #Feature Iteration Controls----
   featnum<-reactiveVal(0) #sets up a reactive variable to allow for iteratation through rows in DFs
   observeEvent(input$iterfeature,{
-    featnum(featnum()+1)#iterates to next feature
-    res<-NULL}) 
+    if(is.null(vals$truths)){
+      if(featnum()>0){
+        df$dt[featnum(),]<-rbind(NA)
+      }
+      featnum(featnum()+1)#iterates to next feature
+    }
+    else(featnum(featnum()+1))
+      }) 
   observeEvent(input$inv_iterfeature,{
     if(featnum()-1>0){
       featnum(featnum()-1)#iterates backwards
       if(df$dt$iteration[featnum()]==featnum() && featnum()>1 && nrow(df$dt)>1 && featnum()%in%(df$dt$iteration)){
         df$dt<-df$dt[-nrow(df$dt),]
       }
-      else({
-        df$dt[1,]<-NA
-      })
+      else(df$dt[featnum(),]<-NA)
     }
     else(return())
     })
   observeEvent(input$nomatch,featnum(featnum()+1)) #skips feature if clicks no match present
   
-  #create reactive groundpoints (to allow for highlighting)
-   gtruth<-observeEvent(input$Ground_Truths_Data,
-     gtruth$buffs<-rep(TRUE,nrow(data$importfile2))
+  #Create 'confidence' table to allow user to enter in confidence value
+  sliderValues<-reactive({
+    data.frame(Name=c("Confidence"),
+               Value=as.character(c(input$integer)),
+               stringsAsFactors=FALSE)
+  })
+  output$values<-renderTable({
+    sliderValues() #confidence values
+  })
+  
+  
+  #Plot & Plot Data Controls----
+  #Define reactive values used throughout code
+  vals<-reactiveValues(
+    chulls=NULL,#initializes these to NULL, but will be updated once running
+    truths=NULL,
+    buffs=NULL
+  )
+  
+  #Initialize vector of T/F for buffers present in data (only runs when importing grtuth data)
+   observeEvent(input$Ground_Truths_Data,
+     vals$buffs<-rep(TRUE,nrow(data$importfile2))#creates vector of TRUE for the #rows in the imported ground truth
    )
   
-  gtruth<-reactiveValues(buffs=NULL)
-   
   #Create a ggplot 'map' that plots the currently selected feature and ground truth w/ centroids
     output$map<-renderPlot({
       if(featnum()==0) return()#will not display a "map" on first iteration where no features exist
       input$map
-    
       
-      allbuffs<-data$importfile2[gtruth$buffs,,drop=FALSE]#stores within current widget all available gtruth buffers
-      selectedbuffs<-data$importfile2[!gtruth$buffs,,drop=FALSE]#stores within current widget all subsetted gtruth buffers
-      
+      allbuffs<-data$importfile2[vals$buffs,,drop=FALSE]#Determines all available gtruth buffers based off of all "TRUE" buffers in vals$buffs vector
+      selectedbuffs<-data$importfile2[!vals$buffs,,drop=FALSE]#Determines all "FALSE" buffers based on those not in vals$buffs
       
       map<-ggplot()+
         geom_sf(data=data$importfile[featnum(),],fill='red')+#add layer for convex hulls
         geom_sf(data=allbuffs,fill='blue')+#add layer for gtruth buffers
         geom_sf(data=selectedbuffs,fill='green')+#add layer for highlighted (selected) gtruth buffers
-        #geom_point(data=data.frame(gtruthdata),aes(x=gtruthdata$NADY,y=gtruthdata$NADY,colour='pink'))+ #plots centroid (must be clicking within 20pixels of this feature to "match" the gtruth)
         coord_sf(datum=st_crs(data$importfile), #sets projection of the ggplot graph
           xlim=c((st_bbox(data$importfile[featnum(),])$xmin)-2,(st_bbox(data$importfile[featnum(),])$xmax)+2), #edits the extents of the graph
           ylim=c((st_bbox(data$importfile[featnum(),])$ymin)-2,(st_bbox(data$importfile[featnum(),])$ymax)+2))
         
       map
     })
+    
 
+    #Plot Interaction Controls----
     observeEvent(input$click,{ #When clicking on a feature
       res<-nearPoints(data.frame(data$importfile2),input$click,xvar='NADX',yvar='NADY',maxpoints=1,threshold=20,allRows=TRUE)#detect a centerpoint of a gtruth buffer within 20 pixels
-      gtruth$buffs<-rep(TRUE,base::nrow(data$importfile2))
-      gtruth$buffs<-xor(gtruth$buffs,res$selected_)#then subset the gtruth data based on this selected centroid
+      vals$buffs<-rep(TRUE,base::nrow(data$importfile2))
+      vals$buffs<-xor(vals$buffs,res$selected_)#then subset the gtruth data based on this selected centroid
+      vals$chulls<-data$importfile[featnum(),]#Makes the reactive chulls values under vals= to the subset of the dataframe based on the current iteration being displayed
+      vals$truths<-data$importfile2[!vals$buffs,,drop=TRUE]#based on a clicked stem, assign the clicked stem (as a subset from the ground truth) to the reactive value under vals
       })
     
-    #Build dataframe based off of clicked ground truth stems
-    vals<-reactiveValues(
-      chulls=NULL,#initializes these to NULL, but will be updated once running
-      truths=NULL
-    )
-    observe({
-      vals$chulls<-data$importfile[featnum(),]#Makes the reactive chulls values under vals= to the subset of the dataframe based on the current iteration being displayed
-    })
-    observeEvent(input$click,{vals$truths<-data$importfile2[!gtruth$buffs,,drop=TRUE]})#based on a clicked stem, assign the clicked stem (as a subset from the ground truth) to the reactive value under vals
-    
+    #Dataframe Controls----
     df<-reactiveValues() #makes a reactive dataframe to store values during matching process
     df$dt<-data.frame(Gtruth_ID=as.numeric(),
                       Gtruth_SP=as.character(),
                       Gtruth_dbh_cm=as.numeric(),
                       Gtruth_NADX=as.numeric(),
-                      GTRUTH_NADY=as.numeric(),
+                      Gtruth_NADY=as.numeric(),
                       Chull_diam=as.numeric(),
                       Confidence=as.integer(),
                       iteration=as.numeric())
@@ -184,18 +191,20 @@ server<-function(input,output,session){
                         Gtruth_NADY=vals$truths$NADY,
                         Chull_diam=vals$chulls$diam,
                         Confidence=sliderValues(),
-                        iteration=featnum())
+                        iteration=featnum()-1)
         df$dt<-rbind(df$dt,row) #binds the row to reactive dataframe
-        df$dt<-df$dt[!duplicated(df$dt$iteration),]
-        #removes "duplicate rows"  that are created during this process to ensure only 1 iteration writes 1 row
+        df$dt<-df$dt[!duplicated(df$dt$iteration),]#removes "duplicate rows"  that are created during this process to ensure only 1 iteration writes 1 row
+        vals$truths<-NULL
       }
     })
+    output$"MatchedTreesTable"<-renderTable(df$dt) #display the dataframe being built (this will be re-rendered and updated at each iteration)
     
+    
+    #Misc. tools / features----
     output$info<-renderText({ #output general text for testing purposes primarily
-      paste0("Selected Tree ID : x=",#gtruthdata[!gtruth$buffs,,drop=FALSE]$treeID, #output window of information
-             "\nCurrent Chull Feature # :",featnum(),
-             "\nteststuff: ",featnum()%in%df$dt$iteration,
-             "\nteststuff2: ")
+      paste0("Selected Tree ID : x= ",data$importfile2[!vals$buffs,,drop=FALSE]$treeID) #output window of information
+      paste0("Current Chull Iteration# :",featnum())
+      shinyjs::logjs(isTRUE(df$dt$iteration[featnum()]==featnum() && featnum()>1 && nrow(df$dt)>1 && featnum()%in%(df$dt$iteration)))
     })
     
     output$downloadData<-downloadHandler(
@@ -204,8 +213,6 @@ server<-function(input,output,session){
       content=function(file){
         write.csv(df$dt,file,row.names=FALSE)
         })
-    
-    output$"MatchedTreesTable"<-renderTable(df$dt) #display the dataframe being built (this will be re-rendered and updated at each iteration)
 }
 
 
