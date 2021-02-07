b356a0f6cd3f1757e59f16e6469921cdd1c39e94
Daniel <38474652+Dspiwak@users.noreply.github.com>
2021-02-06 22:19:40 -0500

    Incorporated the new "resolve chunking" solution to the chunking problem. This requires an initial convex hull generation to be preformed, whereby these chulls are broken up by chunks, and are "remerged" with one another. (See commit noted in the LiDAR_clustering script)

--- LiDAR_main.R
+++ LiDAR_main.R
@@ -32,6 +32,7 @@ source("Scripts/Stem_reconstructions.R")
 
 LiDAR_structure_check("/Processed_Data")
 preprocessed_las<-list.files(path=paste0(wd,"/Raw_Data"),pattern=".laz")
+clipregion<-testarea
 
 if(length(preprocessed_las)>1){ #checks if only 1 .las file or many, currently only implemented for 1 scan file
   print("Too many .laz files present in raw data directory, consider revising (Only applicable following final code)")
@@ -42,33 +43,36 @@ if(length(preprocessed_las)>1){ #checks if only 1 .las file or many, currently o
 
 points_df<-lasdf
 coordinates(points_df)<-c("X","Y") #convert dataframe into spatial points data type
-chunks<-generate_chunks(Fullscan)
-
-offchunk<-generate_offset_chunks(Fullscan)
+chunks<-generate_chunks(Fullscan,chunk_size=sqrt(5000))
 
 
+chunks_with_points<-intersect(chunks,points_df) #extract only the chunks that have points in them...primarily useful for testing
 
-
-
-loop_hulls<-list()
-for(i in 1:length(chunks)){
-  chunk<-chunks[i]
-  clipped_lasdf<-intersect(points_df,chunks)
-  clipped_lasdf<-data.frame(clipped_lasdf)
+#Preform initial runthrough of clustering and convex hull generation of points. 
+clipped_lasdf<-intersect(points_df,chunks_with_points[1])%>%
+  data.frame()
+chunked_hulls<-conv_hulls(cluster_lidar_dbscan(clipped_lasdf,20,0.0067))
+chunked_hulls$chunk_hull_ID<-paste(1,'-',chunked_hulls$clusid)
+for(i in 2:length(chunks_with_points)){
+  chunk<-chunks_with_points[i]
+  clipped_lasdf<-intersect(points_df,chunk) %>%
+    data.frame()
   clustered_points<-cluster_lidar_dbscan(clipped_lasdf,20,0.0067)
-  loop_hulls[i]<-conv_hulls(clustered_points)
+  chunk_hulls<-conv_hulls(clustered_points)
+  chunk_hulls$chunk_hull_ID<-paste(i,'-',chunk_hulls$clusid)
+  chunked_hulls<-rbind(chunked_hulls,chunk_hulls,makeUniqueIDs=TRUE)
 }
 
-temp<-loop_hulls[[1]]
-for(i in 2:length(loop_hulls)){
-temp<-rbind(temp,loop_hulls[[i]],makeUniqueIDs=TRUE)
-}
-writeOGR(temp,dsn=paste0(wd,'/Processed_Data'),layer=paste0(deparse(substitute(temp)),"_AutomatedOuput"),driver="ESRI Shapefile",overwrite_layer=TRUE)
+hulls<-resolve_chunks(chunked_hulls,chunks_with_points) #will "resolve" chunking of convex hulls...but rewrites / recalcs the dbh data
+#could use hulls to "re-extract the points from before to calculate the fourier trans measurment possibly && circle fitting?
+
 
 #####
 ##### LiDAR Processing ( Convex Hulls / Fourier Transform / Matching) ----------
 source("Scripts/Stem_matching.R")
-matched_stems<-match_stems(temp,2.5)
+#A temporary fix (will need to re-write the ID somehow)
+hulls$clusid<-c(1:length(hulls))
+matched_stems<-match_stems(hulls,2.5)
 
 
 #####
@@ -82,7 +86,7 @@ summary(lin_model)
 cat(paste0("With an RMSE of ",rmse," cm",
            "\n Searching in tree sizes >",Smallest.Tree.Size,
            "\n Total trees present:",nrow(Clipped.GTruth),
-           "\n Total trees detected:",nrow(stem_hulls)))
+           "\n Total trees detected:",nrow(hulls)))
 
 p<-ggplot(data=as.data.frame(matched_stems),aes(x=dbh_cm,y=chull_dbh_cm,ymin=0,ymax=max(chull_dbh_cm),xmin=0,xmax=max(dbh_cm)))+
   geom_smooth(method="lm",formula=y~x)+
