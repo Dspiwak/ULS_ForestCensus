bef94508e765375197734fc60d95d7e00ad222e2
Daniel <38474652+Dspiwak@users.noreply.github.com>
2021-02-13 19:33:55 -0500

    Implemented a method of fitting circles to point data based on least squares regression.This method will identify a fitted circles center which will act as an origin to allow for data conversion to different projection spaces (polar coordinates).However this is not optimal for "sparser/ shotgunning" point data. Will be updated to RANSAC when able.
    Implemented conversion ability to polar coordinates. Uses a fitted circle, and will recalculate the circles points in polar coorindates (azimuth and distance). Stores as a dataframe of dataframes. (Inefficient)
    
    Began implementation of fitting fourier series curves to the polar coodinates. To align with Wang et al. article. Currently partially functioning, and has known issues, but return partial results.

--- Scripts/Stem_reconstructions.R
+++ Scripts/Stem_reconstructions.R
@@ -74,3 +74,113 @@ conv_hulls<-function(clusters_df){
   
   return(polys)
 }
+
+calc_circbounds<-function(circle_dat,npoints=100){ #from stack overflow plot ggplot circle https://stackoverflow.com/questions/6862742/draw-a-circle-with-ggplot2
+  r=circle_dat$radius
+  tt<-seq(0,2*pi,length.out=npoints)
+  xx<-circle_dat$cx+r*cos(tt)
+  yy<-circle_dat$cy+r*sin(tt)
+  return(data.frame(x=xx,y=yy))
+} #creates "points" along curve of ellipse...primarily for plotting
+
+fit_circle<-function(points,hulls){ #fits a circle to each of the hulls in a dataset and returns a spatial points dataframe based on the centroids and contains attribute for radius
+  dat<-data.frame("cx"=NA,"cy"=NA,"radius"=NA,"clusid"=NA)
+  for(i in 1:length(hulls)){
+    SingleTree_points<-intersect(points,hulls[i,]) #extracts points from an individual hull when overlapping
+    temp<-data.frame(fitCircle(SingleTree_points$X,SingleTree_points$Y))#fit circle (least squares method...from package...should switch to RANSAC)
+    dat[i,]<-c(temp$cx,temp$cy,temp$radius,hulls$id[[i]])
+    }
+  coordinates(dat)<-c("cx","cy")#makes spatial points data to act as origin for polar coords...radius field can be used as a DBH method technically (although somewhat poor accuracy currently)
+  return(dat)
+}
+
+
+conv2_polar<-function(points_df,Fitted_CircleData,hulls,plot=FALSE){
+  polar_points_dfls<-data.frame("dfs"=matrix(NA,ncol=1,nrow=length(hulls)))
+  for(i in 1:length(hulls)){
+    SingleTree_points<-intersect(points_df,hulls[i,])
+    SingleTree_CircleDat<-Fitted_CircleData[i,]
+    SingleTree_AdjustedCoords<-data.frame(original_x=NA,original_y=NA,dist=NA,azimuth=NA)
+    
+    dist<-NA
+    azimuth<-NA
+    
+    for(k in 1:length(SingleTree_points)){#recalculate the individual tree's point's coords w/ respect to the fitted circle cent. as origin
+      dist<-sqrt(((SingleTree_points[k,]$X-SingleTree_CircleDat$cx)^2)+((SingleTree_points[k,]$Y-SingleTree_CircleDat$cy)^2))
+      azimuth<-atan2((SingleTree_points[k,]$Y-SingleTree_CircleDat$cy),(SingleTree_points[k,]$X-SingleTree_CircleDat$cx))
+      
+      #newx<- (dist*sin(azimuth))
+      #newy<- (dist*cos(azimuth))
+      
+      SingleTree_AdjustedCoords[k,]<-c(SingleTree_points[k,]$X,SingleTree_points[k,]$Y,dist,azimuth)
+    }
+    
+     polar_points_dfls$dfs[i]<-list(SingleTree_AdjustedCoords)
+     polar_points_dfls$hullid[i]<-hulls[i,]$id
+     
+     
+     
+    if(plot){
+      circ<-calc_circbounds(SingleTree_CircleDat)
+      polarplot<-ggplot(data=SingleTree_AdjustedCoords, aes(azimuth,dist))+
+        geom_point()
+      cartplot<-ggplot(data=data.frame(SingleTree_points),aes(X,Y))+
+        geom_point()+
+        geom_point(data=data.frame(SingleTree_CircleDat),aes(cx,cy),col='red')+
+        geom_path(data=circ,aes(x,y),col='red')
+      grid.arrange(cartplot,polarplot,ncol=2,top=paste('Tree ',hulls[i,]$id))
+    }
+  }
+  return(polar_points_dfls)
+}
+ 
+
+
+fit_fourier<-function(AdjustedCoords_dists_df,n,up=10L,plot=FALSE){
+  dat<-AdjustedCoords_dists_df$dist
+  dff=fft(dat)
+  t=seq(from=-pi,to=pi)#may need to adjust to match radians measurment?
+  nt=seq(from=-pi,to=pi-1/up,by=1/up)
+  ndff=array(data=0,dim=c(length(nt),1L))
+  ndff[1]=dff[1]
+  if(n!=0){
+    ndff[2:(n+1)]=dff[2:(n+1)]
+    ndff[length(ndff):(length(ndff)-n+1)]=dff[length(dat):(length(dat)-n+1)]
+  }
+  indff=fft(ndff/max(dat),inverse=TRUE)
+  idff=fft(dff/max(dat),inverse=TRUE)
+
+  ret=data.frame(time=nt,y=Mod(indff))
+  ret$y=((ret$y-min(ret$y)) / (max(ret$y)-min(ret$y)))*(max(dat)-min(dat))+min(dat)
+  #ret$my=Mod(idff)
+  
+  print(paste0)
+  
+  
+  
+  
+  
+  if(plot){
+    #plot(ret$time,ret$y)
+    # if(!add){
+    #   plot(x=t,y=dat,pch=16L,xlab="Time",ylab="Measurment",
+    #        main=ifelse(is.null(main),paste(n," harmonics"),main))
+    #   lines(y=Mod(idff),x=t,col=adjustcolor(1L,alpha=0.5))
+    # }
+    graph<-ggplot(data=AdjustedCoords_dists_df,aes(azimuth,dist))+
+      geom_point()+
+      geom_line(data=ret,aes(time,y),color='red')
+    print(graph)
+    
+  }
+  
+  
+  L=  2#arc length in polar coordinates
+  ds=2*pi #effective domain range (set to 2pi for now because ULS "assumes" equal coverage about face of tree)
+  dbh=(2*L)/ds
+  
+  
+  #using fourier curve and fitted circle to "fill in" fractions of missing areas of fourier curve (where not sufficient data coverage) [currently not implemented]
+  #dbh2=(L/pi)+(1-(ds/2*pi))*pc (where pc is diam of fitted circle)
+  return(dbh)
+}
