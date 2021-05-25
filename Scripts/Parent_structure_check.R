5b95aa8b4fd87b422c1273af5160ed5183edd511
Daniel <38474652+Dspiwak@users.noreply.github.com>
2021-05-25 17:07:04 -0400

    Added a script to check for during initialization.

--- Scripts/Parent_structure_check.R
+++ Scripts/Parent_structure_check.R
@@ -3,7 +3,7 @@
 present_dirs<-list.dirs(path=wd,full.names = FALSE)
 required_dirs<-list("Raw_Data","Scripts","Processed_Data") #Minimum list of required directories to process data
 required_scripts<-list("FGeo_Processing.R","LiDAR_structure_check.R","Parent_structure_check.R","LiDAR_clustering.R","LiDAR_initial_processing.R",
-                       "Stem_reconstructions.R","Stem_matching.R") #list of scripts required to fully preform process
+                       "Stem_reconstructions.R","Stem_matching.R","Stats_Outputs.R") #list of scripts required to fully preform process
 
 for(i in required_dirs){
   cat("\n")
