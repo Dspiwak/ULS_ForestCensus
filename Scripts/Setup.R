b861880f521177281fe9b3c2cb25fc6dcb971fa5
Daniel <38474652+Dspiwak@users.noreply.github.com>
2021-09-14 23:12:53 -0400

    Included New Packages::
    ggpmisc which allows the display of the R2 and line equations on the scatter plots

--- Scripts/Setup.R
+++ Scripts/Setup.R
@@ -3,8 +3,8 @@ setup<-function(){
       "\n     Note: Some packages require 'rtools' or external github calls to specific repositories \n")
   
   required_packages<-c("raster","plyr","dplyr","rgeos","rgdal","lidR","dbscan","grDevices","sp",
-                      "approximator","mapview","ggplot2","ggpubr","grid","MeanShiftR","future",
-                      "maptools","smacof","conicfit","RSEIS","sf","ggforce","ggpointdensity","ggrepel","patchwork")
+                      "approximator","mapview","ggplot2","ggpubr","grid","future",
+                      "maptools","smacof","conicfit","RSEIS","sf","ggforce","ggpointdensity","ggpmisc","ggrepel","patchwork")
   
   install.packages(setdiff(required_packages,rownames(installed.packages())))
   
